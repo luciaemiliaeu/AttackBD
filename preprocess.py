12ef1969ee8cb868a0cf8fbcc32d55a728cb35be
Lúcia Emília <luciaemilia72@gmail.com>
2019-12-13 01:07:34 -0300

    Pre-processamentos

--- preprocess.py
+++ preprocess.py
@@ -1,5 +1,6 @@
 import pandas as pd 
 import numpy as np
+
 '''
 #-- Redução de atributos irrelevantes
 data_attack = pd.read_csv('attacks.csv', sep=';')
@@ -20,29 +21,59 @@ data_attack.drop('mqtt.topic', axis =1, inplace = True)
 data_attack.drop('mqtt.username', axis =1, inplace = True)
 data_attack.drop('mqtt.willmsg', axis =1, inplace = True)
 data_attack.drop('mqtt.willtopic', axis =1, inplace = True)
+data_attack.drop('mqtt.topic_len', axis =1, inplace = True)
 
+data_attack.columns = data_attack.columns.str.lower().str.replace('.', '_')
 data_attack.to_csv('data_attackv2.csv', index=False, sep =';')
+
 
 #-- Balanceamento da base
 data_attack = pd.read_csv('data_attackv2.csv', sep=';')
-attack = data_attack[~(data_attack['label']=='normal')]
-normal = data_attack[(data_attack['label']=='normal')].sample(n = attack.shape[0], random_state=1)
-notselected = data_attack.drop(attack.index)
-notselected = notselected.drop(normal.index)
-data_attack = data_attack.drop(notselected.index)
-
+c = data_attack.groupby('label')
+data_attack = c.apply(lambda x: x.sample(n = c.size().min()).reset_index(drop=True))
 data_attack.to_csv('data_attackv3.csv', index=False, sep =';')
-'''
 
+'''
 
 data_attack = pd.read_csv('data_attackv3.csv', sep=';')
-#data_attack['na'] = pd.isna(data_attack['ipv6_src'])
-#data_attack['virgula'] = data_attack['mqtt_len'].str.contains(',')
-#print(data_attack[(data_attack['virgula'] == True)&(data_attack['na'] == False)])
-#print(data_attack[(data_attack['ipv6_nxt']==58) & (data_attack['label'] != 'normal')])
-#print(data_attack[(data_attack['label'] == 'mitm')])
-print(data_attack['label'].unique())
-#print(data_attack[(data_attack['na'] == True)])
-'''
-data_attack[(data_attack['virgula'] == True)]['mqtt_len'] = 
-'''
+
+# Substituindo o endereço ipv6 pela primeira parte
+data_attack['ipv6_src'] = data_attack.apply(lambda x: x['ipv6_src'].split(":")[0], axis=1)
+data_attack['ipv6_dst'] = data_attack.apply(lambda x: x['ipv6_dst'].split(":")[0], axis=1)
+
+# Substituindo campos pelo número de flags
+index = data_attack[(data_attack['mqtt_dupflag'].isnull() == False)].index
+data_attack.loc[index, 'mqtt_dupflag'] = data_attack.loc[index, :].apply(lambda x: len(str(x['mqtt_dupflag']).split(",")), axis=1)
+index = data_attack[(data_attack['mqtt_hdrflags'].isnull() == False)].index
+data_attack.loc[index, 'mqtt_hdrflags'] = data_attack.loc[index, :].apply(lambda x: len(str(x['mqtt_hdrflags']).split(",")), axis=1)
+index = data_attack[(data_attack['mqtt_msgid'].isnull() == False)].index
+data_attack.loc[index, 'mqtt_msgid'] = data_attack.loc[index, :].apply(lambda x: len(str(x['mqtt_msgid']).split(",")), axis=1)
+index = data_attack[(data_attack['mqtt_msgtype'].isnull() == False)].index
+data_attack.loc[index, 'mqtt_msgtype'] = data_attack.loc[index, :].apply(lambda x: len(str(x['mqtt_msgtype']).split(",")), axis=1)
+index = data_attack[(data_attack['mqtt_retain'].isnull() == False)].index
+data_attack.loc[index, 'mqtt_retain'] = data_attack.loc[index, :].apply(lambda x: len(str(x['mqtt_retain']).split(",")), axis=1)
+
+
+# Substituindo missin values por 0 - ausência de flags nos campos
+index = data_attack[(data_attack['mqtt_dupflag'].isnull() == True)].index
+data_attack.loc[index, 'mqtt_dupflag'] = data_attack.loc[index, :].apply(lambda x: 0, axis=1)
+index = data_attack[(data_attack['mqtt_hdrflags'].isnull() == True)].index
+data_attack.loc[index, 'mqtt_hdrflags'] = data_attack.loc[index, :].apply(lambda x: 0, axis=1)
+index = data_attack[(data_attack['mqtt_msgid'].isnull() == True)].index
+data_attack.loc[index, 'mqtt_msgid'] = data_attack.loc[index, :].apply(lambda x: 0, axis=1)
+index = data_attack[(data_attack['mqtt_msgtype'].isnull() == True)].index
+data_attack.loc[index, 'mqtt_msgtype'] = data_attack.loc[index, :].apply(lambda x: 0, axis=1)
+index = data_attack[(data_attack['mqtt_retain'].isnull() == True)].index
+data_attack.loc[index, 'mqtt_retain'] = data_attack.loc[index, :].apply(lambda x: 0, axis=1)
+index = data_attack[(data_attack['mqtt_kalive'].isnull() == True)].index
+data_attack.loc[index, 'mqtt_kalive'] = data_attack.loc[index, :].apply(lambda x: 0, axis=1)
+#index = data_attack[(data_attack['mqtt_topic_len'].isnull() == True)].index
+#data_attack.loc[index, 'mqtt_topic_len'] = data_attack.loc[index, :].apply(lambda x: 0, axis=1)
+
+# Tratando mqtt
+data_attack['mqtt_len_flag'] = data_attack.loc[:,:].apply(lambda x: len(str(x['mqtt_len']).split(",")), axis=1)
+data_attack['mqtt_len_null'] = data_attack['mqtt_len'].isna()
+data_attack.loc[data_attack[(data_attack['mqtt_len_null']==True)].index,'mqtt_len_flag'] = 0
+data_attack.drop('mqtt_len', axis=1, inplace=True)
+
+data_attack.to_csv('data_attackv4.csv', index=False, sep =',')
