bac509bef0a48113b9e0cf864b4cdb643d735afb
luciaemiliaeu <luciaemilia72@gmail.com>
2019-12-18 15:37:37 -0300

    Adição de métricas de avaliação

--- plot_results.py
+++ plot_results.py
@@ -1,31 +1,30 @@
 import pandas as pd 
 import matplotlib.pyplot as plt 
-
-
-results = pd.read_csv('results.csv', sep =',')
-
-figure = plt.figure(figsize=(27, 9))
-
-results_by_algorit = pd.DataFrame(columns = ['Algoritmo','Pct_treino', 'Acc', 'DV'])
+'''
+results = pd.read_csv('results2.csv', sep =',')
+results_by_algorit = pd.DataFrame(columns = ['Algoritmo','Pct_treino', 'Acc', 'Acc_DV', 'Fscore', 'Precision', 'Recall'])
 
 for pct, values in results.groupby(['Pct_treino']):
 	for alg, data in values.groupby(['Algoritmo']):
-		results_by_algorit.loc[results_by_algorit.shape[0],:] = [alg, pct, data['Acuracia'].mean(), data['Acuracia'].std()]
+		results_by_algorit.loc[results_by_algorit.shape[0],:] = [alg, pct, data['Acuracia'].mean(), data['Acuracia'].std(), data['Fscore'].mean(), data['Precision'].mean(), data['Recall'].mean()]
 results_by_algorit.to_csv('results_by_algorit.csv', sep = ',', index = False)
-
+'''
 results = pd.read_csv('results_by_algorit.csv', sep =',')
 
-for alg, values in results.groupby(['Algoritmo']):
-	print(alg)
-	plt.plot(values['Pct_treino'], values['Acc'], label = alg)
-	plt.fill_between(values['Pct_treino'], values['Acc']-2*values['DV'],values['Acc']+2*values['DV'], alpha=0.2 )
+metrics = ['Accuracy', 'F-score', 'Precision', 'Recall' ]
+for metric in metrics:	
+	fig = plt.figure()
+	plt.title(metric)
+	for alg, values in results.groupby(['Algoritmo']):
+		if metric == 'Accuracy':
+			plt.plot(values['Pct_treino'], values[metric], label = alg)
+			plt.fill_between(values['Pct_treino'], values[metric]-2*values['Acc_DV'],values[metric]+2*values['Acc_DV'], alpha=0.2 )
+		else:		
+			plt.plot(values['Pct_treino'], values[metric], label = alg)
+	plt.axis([results['Pct_treino'].min(),results['Pct_treino'].max(), 0, 1.0])
+	plt.xlabel('Train size')
+	plt.ylabel('Accuracy')
+	plt.grid(True)
+	plt.legend()
 
-plt.axis([results['Pct_treino'].min(),results['Pct_treino'].max(), 0, 1.0])
-plt.xlabel('Train size')
-plt.ylabel('Accuracy')
-plt.grid(True)
-
-plt.legend()
 plt.show()
-
-plt.savefig('resultado')
--- processing.py
+++ processing.py
@@ -1,40 +1,56 @@
 import pandas as pd 
 import numpy as np 
-import keras
-from keras.models import Sequential
-from keras.layers import Dense, Dropout, Activation
-from keras.optimizers import SGD
+from sklearn.neural_network import MLPClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import SVC
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn.metrics import accuracy_score, f1_score, precision_score, recall_score
 
 # import dataset
 data = pd.read_csv('data_attackv5.csv', sep =',')
 X = data.drop('label', axis=1)
 y = data['label']
 
-X_test = X.sample(frac=0.4)
-y_test = keras.utils.to_categorical(y.loc[X_test.index].values, num_classes=5)
+# modelos 
+names = ["Nearest Neighbors", "RBF SVM",
+		 "Decision Tree", "Random Forest","Naive Bayes", "Neural Net"]
 
-X_train = X.drop(X_test.index)
-y_train = keras.utils.to_categorical(y.loc[X_train.index].values, num_classes=5)
+classifiers = [
+	KNeighborsClassifier(3),
+	SVC(gamma=2, C=1),
+	DecisionTreeClassifier(max_depth=5),
+	RandomForestClassifier(max_depth=5, n_estimators=10, max_features=1),
+	GaussianNB(),
+	MLPClassifier(alpha=1, max_iter=1000),
+	]
 
-X_test = X_test.values
-X_train = X_train.values
+# Testes 
+results = pd.DataFrame(columns =['Pct_treino', 'Algoritmo', 'Acuracia', 'Fscore', 'Precision', 'Recall'])
+pct = 0.1
+while pct<= 0.9:
+	print('training with ', str(pct) , "%...")
+	for ext in range(10):
+		X_test = X.sample(frac=pct)
+		y_test = y.loc[X_test.index].values
 
-#creating the model
-model = Sequential()
-model.add(Dense(64, activation='relu', input_dim=X.shape[1]))
-model.add(Dropout(0.5))
-model.add(Dense(64, activation='relu'))
-model.add(Dropout(0.5))
-model.add(Dense(5, activation='softmax'))
+		X_train = X.drop(X_test.index)
+		y_train = y.loc[X_train.index].values
 
-sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
+		X_test = X_test.values
+		X_train = X_train.values
 
-model.compile(loss='categorical_crossentropy',
-              optimizer=sgd,
-              metrics=['accuracy'])
+		for name, clf in zip(names, classifiers):
+			clf.fit(X_train, y_train)
+			y_predicted = clf.predict(X_test)
+			score = accuracy_score(y_test, y_predicted)
+			f1 = f1_score(y_test, y_predicted, average='weighted')
+			precision = precision_score(y_test, y_predicted, average='weighted')
+			recall = recall_score(y_test, y_predicted, average='weighted')
+			results.loc[results.shape[0],:] = [pct, name, score, f1, precision, recall]
+			print(ext, name, score)
+	pct += 0.1
 
-model.fit(X_train, y_train,epochs=10)
+results.to_csv('results2.csv',index=False, sep =',')
 
-score = model.evaluate(X_test, y_test)
-
-print(score)
